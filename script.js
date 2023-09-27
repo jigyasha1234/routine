aa60512a3ecd52692f65cbe5897081f3258837ed
jigyasha rani <jigrani0000@gmail.com>
2023-09-27 21:23:12 +0530

    adding filter feature

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const list = document.getElementById("todo-list");
 const form = document.getElementById("todo-form");
 const todoText = document.getElementById("todo-text");
-const clearButton =document.querySelector(".clearButton");
+const clearButton = document.querySelector(".clearButton");
 const totalTasks = document.querySelector(".total-tasks");
+const incompleteButton = document.querySelector(".incompleteButton");
+const completeButton = document.querySelector(".completeButton");
+const allButton = document.querySelector(".allButton");
 
 let todos = [];
 
 function saveTodosInMemory() {
   console.log("saving in memory");
-  localStorage.setItem("todosInMemory", JSON.stringify(todos));  
+  localStorage.setItem("todosInMemory", JSON.stringify(todos));
 }
 
 function renderTodos() {
@@ -23,20 +26,76 @@ function renderTodos() {
   });
 }
 
+function completeRenderTodos() {
+  // empty HTML inside the list
+  list.innerHTML = "";
+
+  // loop over all todos
+  todos.forEach((todo) => {
+    if (todo.complete === false && todo.isDeleted === 0) {
+      renderTodo(todo);
+    }
+  });
+}
+
+function incompleteRenderTodos() {
+  // empty HTML inside the list
+  list.innerHTML = "";
+
+  // loop over all todos
+  todos.forEach((todo) => {
+    if (todo.complete === true && todo.isDeleted === 0) {
+      renderTodo(todo);
+    }
+  });
+}
+
+function completeTodo(removeId) {
+  //Find index of specific object using findIndex method.
+  todoIndex = todos.findIndex((todo) => todo.id == removeId);
+
+  //Update object's complete property.
+  todos[todoIndex].complete = true;
+}
+
+function clearAllTodos() {
+  // deleting all todos
+  todos.forEach((todo) => {
+    if (todo.isDeleted === 0) {
+      todo.isDeleted = 1;
+    }
+  });
+  // Just to re render
+  saveTodosInMemory();
+  renderTodos();
+}
+
+function deleteTodo(removeId) {
+  //Find index of specific object using findIndex method.
+  todoIndex = todos.findIndex((todo) => todo.id == removeId);
+
+  //Update object's isSeleted property.
+  todos[todoIndex].isDeleted = 1;
+
+  // Just to re render
+  saveTodosInMemory();
+  renderTodos();
+}
+
 function renderTodo(todo) {
   list.innerHTML += `
         <li style="font-size: 30px">
           <div class="form-check" style="display: flex;">
           <div style="margin: 0px">
-            <input onclick="completeTodo(${todo.id});" class="form-check-input" type="checkbox" />
-            <label class="form-check-label" for="flexCheckDefault">
+            <input onclick="completeTodo(${todo.id});" type="checkbox" id="${todo.id}" name="${todo.id}" />
+            <label class="form-check-label" for="${todo.id}">
               ${todo.text}
             </label>
             </div>            
             <div onclick="deleteTodo(${todo.id});" style='font-size:20px; margin-right: 0px'>&#10006;</div>            
           </div>
         </li>
-      `;          
+      `;
 }
 
 function addTodo(todo) {
@@ -48,41 +107,6 @@ function addTodo(todo) {
   });
   renderTodo(todo);
   saveTodosInMemory();
-}
-
-function completeTodo(removeId) {
-  //Find index of specific object using findIndex method.
-  todoIndex = todos.findIndex((todo) => todo.id == removeId);
-
-  //Update object's complete property.
-  todos[todoIndex].complete = true;  
-
-}
-
-function clearAllTodos() { 
-
-  // deleting all todos
-  todos.forEach((todo) => {
-    if (todo.isDeleted === 0) {
-      todo.isDeleted=1;
-    }
-  });
-
-  // Just to re render
-  saveTodosInMemory();
-  renderTodos();
-}
-
-function deleteTodo(removeId) {
-  //Find index of specific object using findIndex method.
-  todoIndex = todos.findIndex((todo) => todo.id == removeId);
-
-  //Update object's isSeleted property.
-  todos[todoIndex].isDeleted = 1;  
-
-  // Just to re render
-  saveTodosInMemory();
-  renderTodos();
 }
 
 // listen for form to be submitted
@@ -102,5 +126,8 @@ form.addEventListener("submit", (e) => {
 
 // on page load - the following functions will run
 todos = JSON.parse(localStorage.getItem("todosInMemory")) || [];
-clearButton.innerHTML +=`<button onclick="clearAllTodos();"> Clear All</button>`;   
+clearButton.innerHTML += `<button onclick="clearAllTodos();"> Clear All</button>`;
+incompleteButton.innerHTML += `<button onclick="incompleteRenderTodos();"> Completed list</button>`;
+completeButton.innerHTML += `<button onclick="completeRenderTodos();"> Incomplete list</button>`;
+allButton.innerHTML += `<button onclick="renderTodos();"> All list </button>`;
 renderTodos();
